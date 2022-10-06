ec1b8cbc96a6cc974747239a286904ac524412cd
Mathis Ravera <mravera@student.42lausanne.ch>
2022-10-06 15:51:46 +0200

    first

---
+++ so_long.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mravera <mravera@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/06 15:21:16 by mravera           #+#    #+#             */
+/*   Updated: 2022/10/06 15:50:26 by mravera          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <mlx.h>
+
+typedef struct    data_s
+{
+    void          *mlx_ptr;
+    void          *mlx_win;
+}                 data_t;
+
+int main(void)
+{
+    data_t        data;
+
+    if ((data.mlx_ptr = mlx_init()) == NULL)
+        return (EXIT_FAILURE);
+    if ((data.mlx_win = mlx_new_window(data.mlx_ptr, 640, 480, "Hello world")) == NULL)
+        return (EXIT_FAILURE);
+    mlx_loop(data.mlx_ptr);
+    return (EXIT_SUCCESS);
+}
